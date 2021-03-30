b38302782ee4fabcdfa8d7e83ddaaa86f7f0c179
gerardomuriasservicenow <gerardo.murias@servicenow.com>
2021-03-30 10:22:43 +0200

    -

--- main.tf
+++ main.tf
@@ -24,8 +24,8 @@ resource "azurerm_resource_group" "mysqlresourcegroup" {
 
 resource "azurerm_mysql_server" "mysqlserverinstance1" {
   name                = "mysqlserverinstance1"
-  location            = azurerm_resource_group.mysqlresourcegroup.location
-  resource_group_name = azurerm_resource_group.mysqlresourcegroup.name
+  location            = azurerm_resource_group.exampleresources.location
+  resource_group_name = azurerm_resource_group.exampleresources.name
 
   administrator_login          = "mysqladminun"
   administrator_login_password = "H@Sh1CoR3!"
