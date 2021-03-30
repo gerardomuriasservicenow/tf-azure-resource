12384a47a72f46e8b8b8afdbf8d809ab4b16748a
gerardomuriasservicenow <gerardo.murias@servicenow.com>
2021-03-30 17:05:14 +0200

    -

--- main.tf
+++ main.tf
@@ -1,21 +1,16 @@
-# Configure the Azure provider
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source = "hashicorp/azurerm"
-#       version = ">= 2.26"
-#     }
-#   }
-# }
+#Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
 
-# provider "azurerm" {
-#   features {}
-# }
-
-#resource "azurerm_resource_group" "rg" {
-#  name     = "myTFResourceGroup"
-#  location = "westus2"
-#}
+provider "azurerm" {
+  features {}
+}
 
 resource "azurerm_resource_group" "gerardomysqlresourcegroup" {
   name     = "gerardomysqlresourcegroup"
