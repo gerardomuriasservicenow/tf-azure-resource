0787bc50a9d808470c9902ff276709823beda204
gerardomuriasservicenow <gerardo.murias@servicenow.com>
2021-03-30 09:39:17 +0200

    -

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_mysql_server" "example" {
-  name                = "mySqlServerInstance1"
+  name                = "example-mysqlserver"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
