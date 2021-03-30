06f09950672a26f04517f01e1665861d61ec9c94
gerardomuriasservicenow <gerardo.murias@servicenow.com>
2021-03-30 14:35:16 +0200

    -

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "gerardomysqlresourcegroup" {
 }
 
 resource "azurerm_mysql_server" "${var.stack_name}" {
-  name                = "${var.stack_name}"
+  name                = var.stack_name
   location            = azurerm_resource_group.gerardomysqlresourcegroup.location
   resource_group_name = azurerm_resource_group.gerardomysqlresourcegroup.name
 
