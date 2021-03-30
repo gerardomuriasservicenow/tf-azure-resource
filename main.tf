410f6dd17b2f05a30546102569b64cd0f6a105ab
gerardomuriasservicenow <gerardo.murias@servicenow.com>
2021-03-30 16:44:42 +0200

    -

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 # Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = ">= 2.26"
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
 #resource "azurerm_resource_group" "rg" {
 #  name     = "myTFResourceGroup"
